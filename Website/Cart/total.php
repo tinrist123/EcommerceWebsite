ab90273cd03552071df9a214f05e07a012ed8e03
Huy Quynh <huyquynh11to@gmail.com>
2020-06-08 13:53:21 +0700

    Quynh

---
+++ Website/Cart/total.php
@@ -0,0 +1,14 @@
+<?php
+include "../header/header.html"
+?>
+<header>
+    <link rel="stylesheet" href="index.css">
+</header>
+<!-- <main> -->
+<?php
+include "./index.html";
+?>
+<!-- </main> -->
+<?php
+include "../footer/footer.html";
+?>
